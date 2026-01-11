43c1f9132278da4a8b78875ba30704908e6d8a2d
mosodi007 <martins.osodi@gmail.com>
2026-01-11 19:55:21 +0100

    Updated vite.config.ts

--- src/services/pdfImageExtractor.ts
+++ src/services/pdfImageExtractor.ts
@@ -1,6 +1,6 @@
 import * as pdfjsLib from 'pdfjs-dist';
 
-pdfjsLib.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjsLib.version}/build/pdf.worker.min.mjs`;
+pdfjsLib.GlobalWorkerOptions.workerSrc = `https://cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjsLib.version}/build/pdf.worker.min.mjs`;
 
 export interface PageImage {
   pageNumber: number;
